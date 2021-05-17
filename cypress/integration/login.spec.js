0ff14afb9d9e73365e1be76721890ba7b04dfb25
Mery <maria.juan.vi@gmail.com>
2021-05-17 19:21:22 +0200

    Tests E2E

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,67 @@
+//Login - María
+var pass = 'Chubaca2020';
+var emailValido = 'pepe@gmail.com';
+var emailInvalido = 'pepito@gmail.com';
+
+describe('Login', () => {
+    
+    beforeEach(() => {
+        // Abrimos la web
+        cy.visit('http://localhost:4200');
+        // Click botón registro
+        cy.get('#btn-login').click();
+    });
+
+    it('Validación correcta', () => {
+        // Submit del formulario
+        cy.get('#login').submit();
+
+        // Se muestran los mensajes de validación
+        cy.contains('Este campo es obligatorio.');
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+    it('Login correcto', () => {
+        // Email
+        cy.get('#email').type(emailValido).should('have.value', emailValido);
+
+        // Contraseña
+        cy.get('#pass').type(pass).should('have.value', pass);
+
+        // Submit del formulario
+        cy.get('#login').submit();
+
+        // La url ahora debe contener /home
+        cy.url().should('include', '/home');
+
+        // Espera 1s
+        cy.wait(1000);
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+    it('Login incorrecto', () => {
+        // Email
+        cy.get('#email').type(emailInvalido).should('have.value', emailInvalido);
+
+        // Contraseña
+        cy.get('#pass').type(pass).should('have.value', pass);
+
+        // Submit del formulario
+        cy.get('#login').submit();
+
+        // Alerta login incorrecto
+        cy.contains('No se ha podido iniciar sesión. Revise sus credenciales.');
+
+        // Espera 1s
+        cy.wait(1000);
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+
+});
---
+++ cypress/integration/registro.spec.js
@@ -0,0 +1,77 @@
+//Registro - María
+var nombre = 'Maria2';
+var email = 'maria2@gmail.com';
+var pass = 'Chubaca2020';
+
+describe('Registro', () => {
+
+    beforeEach(() => {
+        // Abrimos la web
+        cy.visit('http://localhost:4200');
+        // Click botón registro
+        cy.get('#btn-registro').click();
+    });
+
+
+    it('Validación correcta ', () => {
+        // Submit del formulario
+        cy.get('#registro').submit();
+
+        // Se muestran los mensajes de validación
+        cy.contains('Este campo es obligatorio.');
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+    it('Registro correcto', () => {
+        // Email
+        cy.get('#email').type(email).should('have.value', email);
+
+        // Nombre
+        cy.get('#name').type(nombre).should('have.value', nombre);
+
+        // Contraseña
+        cy.get('#pass').type(pass).should('have.value', pass);
+
+        // Confirmación contraseña
+        cy.get('#confirmPass').type(pass).should('have.value', pass);
+
+        // Submit del formulario
+        cy.get('#registro').submit();
+
+        // La url ahora debe contener /home
+        cy.url().should('include', '/home');
+
+        // Espera 2s
+        cy.wait(2000);
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+    it('Correo ya registrado', () => {
+        // Email
+        cy.get('#email').type('mar@gmail.com').should('have.value', 'mar@gmail.com');
+
+        // Nombre
+        cy.get('#name').type('Mar').should('have.value', 'Mar');
+
+        // Contraseña
+        cy.get('#pass').type('Chubaca2020').should('have.value', 'Chubaca2020');
+
+        // Confirmación contraseña
+        cy.get('#confirmPass').type('Chubaca2020').should('have.value', 'Chubaca2020');
+
+        // Submit del formulario
+        cy.get('#registro').submit();
+
+        // Alerta correo ya registrado
+        cy.contains('El correo electrónico introducido ya está registrado.');
+
+        // Hacemos captura de pantalla
+        cy.screenshot();
+    });
+
+
+});

8cce035cb124d7d77af41f0a222771d27fec570e
Mery <maria.juan.vi@gmail.com>
2021-05-22 17:16:09 +0200

    Perfil & menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // Componentes
 import { MenuComponent } from './menu/menu.component';
@@ -61,6 +62,7 @@ import { ConfirmModalComponent } from './views/confirm-modal/confirm-modal.compo
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    NgbModule,
     // Firebase
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireModule,
--- src/app/views/perfil/perfil.component.ts
+++ src/app/views/perfil/perfil.component.ts
@@ -91,10 +91,10 @@ export class PerfilComponent implements OnInit {
         this.enableDisableUpdateEmail(2);
         this.email.reset();
       })
-      .catch(error => {        
+      .catch(error => {
         if (error.code === 'auth/requires-recent-login') {
           this.msg = 'Debes relogearte para poder cambiar el email.';
-        }else if(error.code === 'auth/email-already-in-use') {
+        } else if (error.code === 'auth/email-already-in-use') {
           this.msg = 'El email introducido ya está registrado';
         }
       });
@@ -129,6 +129,8 @@ export class PerfilComponent implements OnInit {
   enableDisableUpdateName(type: number) {
     if (type == 1) {
       this.enabledName = true;
+      this.enabledPass = false;
+      this.enabledEmail = false;
     } else {
       this.enabledName = false;
     }
@@ -142,6 +144,7 @@ export class PerfilComponent implements OnInit {
     if (type == 1) {
       this.enabledEmail = true;
       this.enabledPass = false;
+      this.enabledName = false;
     } else {
       this.enabledEmail = false;
       this.msg = '';
@@ -156,10 +159,14 @@ export class PerfilComponent implements OnInit {
     if (type == 1) {
       this.enabledPass = true;
       this.enabledEmail = false;
+      this.enabledName = false;
     } else {
       this.enabledPass = false;
       this.msg = '';
     }
   }
 
+  openFileSelection() {
+    document.getElementById('file').click();
+  }
 }
